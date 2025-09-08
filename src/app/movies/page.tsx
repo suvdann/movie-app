e5260cc226117877b3dba9d260426e6d8fcd8c78
suvdann <m.erdenesuwd2021@gmail.com>
2025-09-08 21:29:44 +0800

    last

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -19,5 +19,4 @@ const Home = () => {
     </div>
   );
 };
-
 export default Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { Hero } from "./movies/_components/Hero";
-import { PopularMovies } from "./movies/_components/PopularMovies";
-import { TopRatedMovies } from "./movies/_components/TopRatedMovies";
-import { UpcomingMovies } from "./movies/_components/UpcomingMovies";
-
-const Home = () => {
-  return (
-    <div className="w-full ">
-      <Hero />
-
-      <div className="lg:px-20">
-        <UpcomingMovies />
-        <PopularMovies />
-        <TopRatedMovies />
-      </div>
-    </div>
-  );
-};
-export default Home;
+export default function RootPage() {
+  redirect("/movies");
+}
